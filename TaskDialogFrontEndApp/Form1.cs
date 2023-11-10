8322c637287417b8bbc4b635e457ffded0165183
Karen Payne <paynekaren@comcast.net>
2023-11-10 03:09:57 -0800

    refactor

--- TaskDialogFrontEndApp/Form1.cs
+++ TaskDialogFrontEndApp/Form1.cs
@@ -39,8 +39,24 @@ namespace TaskDialogFrontEndApp
         private void Question3Button_Click(object sender, EventArgs e)
         {
             var message = "Verify you understand the terms presented.";
-            var expandText = "Terms of agreement are a set of legal conditions used to ensure that all parties involved in a contract or transaction understand the responsibilities and obligations of each party. They can be found in any type of business document, including service contracts, purchase orders, and sales agreements. Having a thorough understanding of terms of agreement is essential for any business owner or professional who wishes to make sure their rights and interests are protected. In this blog post, let’s explore what terms of agreement actually mean and why it’s so important to get them right every time.";
-            var (answer, verification) = Dialogs.Question(this, message, "Action required",expandText,true, DialogResult.No);
+            
+            // better placed in a file or resource
+            var expandText = 
+                "Terms of agreement are a set of legal conditions " +
+                "used to ensure that all parties involved in a contract or " +
+                "transaction understand the responsibilities and obligations " +
+                "of each party. They can be found in any type of business document, " +
+                "including service contracts, purchase orders, and sales " +
+                "agreements. Having a thorough understanding of terms of " +
+                "agreement is essential for any business owner or " +
+                "professional who wishes to make sure their rights " +
+                "and interests are protected. In this blog post, let’s " +
+                "explore what terms of agreement actually mean and why it’s " +
+                "so important to get them right every time.";
+
+            var (answer, verification) = Dialogs.Question(
+                this, message, "Action required", expandText,true, DialogResult.No);
+
             if (answer && verification)
             {
                 Dialogs.Information(this,"Verified");
